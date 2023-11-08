799f4443dcc772333bafc3b2369dec267c26d220
Michael Novotny <manovotny@gmail.com>
2023-11-08 11:24:40 -0600

    Fixes boundary

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { SingleProduct } from '#/components/single-product';
 import { CartCountProvider } from '#/components/cart-count-context';
 import { Header } from '#/components/header';
 import { Ping } from '#/components/ping';
+import { Boundary } from '#/components/boundary';
 import { Suspense } from 'react';
 
 export default async function Page() {
@@ -30,65 +31,67 @@ export default async function Page() {
           <li>Try refreshing the page to restart the demo.</li>
         </ul>
       </div>
-      <CartCountProvider>
-        <div className="space-y-10">
-          <Header />
+      <Boundary animateRerendering={false} labels={['Demo']} size="small">
+        <CartCountProvider>
+          <div className="space-y-10">
+            <Header />
 
-          <div className="space-y-8 lg:space-y-14">
-            {/* @ts-expect-error Async Server Component */}
-            <SingleProduct
-              data={fetch(
-                `https://app-router-api.vercel.app/api/products?id=1`,
-              )}
-            />
-
-            <div className="relative">
-              <div className="absolute -left-4 top-2">
-                <Ping />
-              </div>
-            </div>
-
-            <Suspense fallback={<RecommendedProductsSkeleton />}>
+            <div className="space-y-8 lg:space-y-14">
               {/* @ts-expect-error Async Server Component */}
-              <RecommendedProducts
-                path="/streaming/node/product"
+              <SingleProduct
                 data={fetch(
-                  // We intentionally delay the reponse to simulate a slow data
-                  // request that would benefit from streaming
-                  `https://app-router-api.vercel.app/api/products?delay=500&filter=1`,
-                  {
-                    // We intentionally disable Next.js Cache to better demo
-                    // streaming
-                    cache: 'no-store',
-                  },
+                  `https://app-router-api.vercel.app/api/products?id=1`,
                 )}
               />
-            </Suspense>
 
-            <div className="relative">
-              <div className="absolute -left-4 top-2">
-                <Ping />
+              <div className="relative">
+                <div className="absolute -left-4 top-2">
+                  <Ping />
+                </div>
               </div>
-            </div>
 
-            <Suspense fallback={<ReviewsSkeleton />}>
-              {/* @ts-expect-error Async Server Component */}
-              <Reviews
-                data={fetch(
-                  // We intentionally delay the reponse to simulate a slow data
-                  // request that would benefit from streaming
-                  `https://app-router-api.vercel.app/api/reviews?delay=1000`,
-                  {
-                    // We intentionally disable Next.js Cache to better demo
-                    // streaming
-                    cache: 'no-store',
-                  },
-                )}
-              />
-            </Suspense>
+              <Suspense fallback={<RecommendedProductsSkeleton />}>
+                {/* @ts-expect-error Async Server Component */}
+                <RecommendedProducts
+                  path="/streaming/node/product"
+                  data={fetch(
+                    // We intentionally delay the reponse to simulate a slow data
+                    // request that would benefit from streaming
+                    `https://app-router-api.vercel.app/api/products?delay=500&filter=1`,
+                    {
+                      // We intentionally disable Next.js Cache to better demo
+                      // streaming
+                      cache: 'no-store',
+                    },
+                  )}
+                />
+              </Suspense>
+
+              <div className="relative">
+                <div className="absolute -left-4 top-2">
+                  <Ping />
+                </div>
+              </div>
+
+              <Suspense fallback={<ReviewsSkeleton />}>
+                {/* @ts-expect-error Async Server Component */}
+                <Reviews
+                  data={fetch(
+                    // We intentionally delay the reponse to simulate a slow data
+                    // request that would benefit from streaming
+                    `https://app-router-api.vercel.app/api/reviews?delay=1000`,
+                    {
+                      // We intentionally disable Next.js Cache to better demo
+                      // streaming
+                      cache: 'no-store',
+                    },
+                  )}
+                />
+              </Suspense>
+            </div>
           </div>
-        </div>
-      </CartCountProvider>
+        </CartCountProvider>
+      </Boundary>
     </>
   );
 }
