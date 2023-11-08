1b621836ce39010b1f335550eb6f22705c334078
Michael Novotny <manovotny@gmail.com>
2023-11-08 11:56:26 -0600

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,13 @@ import { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://app-router.vercel.app/'),
-  title: {
-    default: 'Next.js App Router',
-    template: '%s | Next.js App Router',
-  },
-  description:
-    'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
+  metadataBase: new URL('https://nextjs-partial-prerendering.vercel.app/'),
+  title: 'Next.js Partial Prerendering',
+  description: 'A demo of Next.js using Partial Prerendering.',
   openGraph: {
-    title: 'Next.js App Router Playground',
-    description:
-      'A playground to explore new Next.js App Router features such as nested layouts, instant loading states, streaming, and component level data fetching.',
-    images: [`/api/og?title=Next.js App Router`],
+    title: 'Next.js Partial Prerendering',
+    description: 'A demo of Next.js using Partial Prerendering.',
+    images: [`/api/og?title=Next.js Partial Prerendering`],
   },
   twitter: {
     card: 'summary_large_image',
