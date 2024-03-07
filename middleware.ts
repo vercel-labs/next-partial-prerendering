ece90e58ed3f4eea0080051d12c2f3cd93d54403
Dodie Moore <drmoore324@gmail.com>
2024-03-07 12:44:07 -0500

    remove

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse, userAgent } from 'next/server';
+import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
