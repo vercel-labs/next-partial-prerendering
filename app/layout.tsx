c62aca2f71b710271898baf951cfbd7598b7a268
Dodie Moore <drmoore324@gmail.com>
2024-03-07 14:31:39 -0500

    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en" className={`[color-scheme:dark] ${GeistSans.variable}`}>
       <body className="overflow-y-scroll bg-gray-1100 pb-36">
         <div>
+          HI
           <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
             <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
               <div className="rounded-lg bg-black p-3.5 lg:p-6">
